034f78e16ed8cce3b86c465e5ff9fd059e8235f4
Peter Adams <peter.adams@ecitsolutions.no>
2026-02-01 18:00:53 +0100

    Add lowercase :sqlit command

--- plugin/sqlit.lua
+++ plugin/sqlit.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command("Sqlit", function()
   require("sqlit").open()
 end, { desc = "Open sqlit" })
+
+vim.api.nvim_create_user_command("sqlit", function()
+  require("sqlit").open()
+end, { desc = "Open sqlit" })
